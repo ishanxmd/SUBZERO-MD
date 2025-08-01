144e7d875829abc9def160cef31d2789c3490683
ishanxmd <ishanmadusanke548@gmail.com>
2025-08-01 18:14:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;WQ1BQTBI#AjrGn5tUsj0LLmThFTxsh0muB_LHfwNeaDDpaGZhC7M", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "94778761926", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
+// CREATED BY DARRELL MUCHERI ( MR ISHAN-X 🇱🇰 )
